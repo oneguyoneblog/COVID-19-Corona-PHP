0b979cec1674325bba9c170b06fe2ca81cf0aaae
One Guy, One Blog <OneGuy-OneBlog@users.noreply.github.com>
2020-03-21 17:45:07 +0100

    Update COVID-19-Corona-PHP.php

--- COVID-19-Corona-PHP.php
+++ COVID-19-Corona-PHP.php
@@ -8,7 +8,6 @@
  * 
  **/
 
-
  $url = "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?where=UPPER(Country_Region)%20like%20%27%25INDONESIA%25%27&outFields=Last_Update,Recovered,Deaths,Confirmed&returnGeometry=false&outSR=4326&f=json";
  
  $client = curl_init($url);
@@ -33,7 +32,7 @@
  echo "<br>";
  echo "Deaths: ".$Deaths;
  echo "<br>";
- echo "Revocered: ".$Recovered;
+ echo "Recovered: ".$Recovered;
  echo "<br>";
  echo "Last Update: ".$Last_Update;
 
